96c2b66a06d85417c3a81fc8d25fbf6d7409f1eb
ScriptSaga03 <mehtab185198131@gmail.com>
2024-12-11 18:48:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/todu-app/', // Replace this with your repository name
+  base: '/todu-app/', // Replace this with your GitHub repository name
   plugins: [react()],
 });
